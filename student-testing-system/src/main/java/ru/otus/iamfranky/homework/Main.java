210864fed82a3e61651f4e152e70ef461358900e
Murashko, Fedor <fedor.murashko@x5.ru>
2020-09-07 02:16:15 +0300

    some ref

--- student-testing-system/src/main/java/ru/otus/iamfranky/homework/Main.java
+++ student-testing-system/src/main/java/ru/otus/iamfranky/homework/Main.java
@@ -10,16 +10,16 @@ public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(CONTEXT_PATH);
-//        try {
-//            context = new ClassPathXmlApplicationContext(CONTEXT_PATH);
+        try {
+            context = new ClassPathXmlApplicationContext(CONTEXT_PATH);
             context.refresh();
 
             var app = new App(context.getBean(StudentService.class),
                     context.getBean(ExamService.class));
 
             app.start();
-//        } finally {
-//            context.close();
-//        }
+        } finally {
+            context.close();
+        }
     }
 }
